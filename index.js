3f3222ccbc3b18ee3b67c73ada2550fa033e18a7
Nnatuanya Frank <nnatuanyafrank@gmail.com>
2024-09-11 22:17:00 +0100

    ok

---
+++ index.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const animatedText = document.getElementsByClassName('head');
+    animatedText.style.aminationPlayState = 'running';
+})
